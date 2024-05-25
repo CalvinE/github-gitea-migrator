19487ad2dca89eecdb2803cf328d79b506ae7be4
CalvinE <calvin.echols@gmail.com>
2024-05-25 13:04:14 -0400

    minor cleanup

--- index.js
+++ index.js
@@ -15,6 +15,7 @@ function sleep(ms) {
 
 /**
   * This function migrates all github reops for a given user over to gitea as a mirror.
+  * This uses the search/repositories endpoint so it will get any private repos the user has if the access token used has access to them.
   *
   * @param {string} username 
   * @param {string} ghToken 
@@ -82,61 +83,6 @@ let numIters = 0;
 let numRepos = 0;
 async function run() {
   await mirrorGithubUserReposInGitea(process.env.GITHUB_USERNAME, process.env.GITHUB_TOKEN, process.env.GITEA_URL, process.env.GITEA_TOKEN);
-  // const ghClient = new Octokit({
-  //   auth: process.env.GITHUB_TOKEN
-  // });
-  // const gtClient = giteaApi(process.env.GITEA_URL, {
-  //   token: process.env.GITEA_TOKEN,
-  // });
-  // const githubRepos = [];
-  // await ghClient.paginate("GET /search/repositories", { q: `user:${process.env.GITHUB_USERNAME}`, per_page: 30 }, (response, done) => {
-  //   numIters++;
-  //   // console.dir(response);
-  //   response.data.map((repo) => {
-  //     numRepos++;
-  //     // console.log(repo);
-  //     githubRepos.push(repo);
-  //   });
-  // });
-  // for (const ghRepo of githubRepos) {
-  //   console.log("migrating repo", ghRepo.name);
-  //   try {
-  //     const gtRepoResponse = await gtClient.repos.repoGet(process.env.GITEA_USERNAME, ghRepo.name);
-  //     console.log("repo must exist, did not get error...", ghRepo.name, gtRepoResponse);
-  //   } catch (err) {
-  //     if (err.status === 404) {
-  //       // Repo does not exist in Gitea, migrate it...
-  //       const migrateResponse = await gtClient.repos.repoMigrate({
-  //         service: "github",
-  //         auth_token: process.env.GITHUB_TOKEN,
-  //         clone_addr: ghRepo.html_url,
-  //         repo_name: ghRepo.name,
-  //         description: ghRepo.description,
-  //         issues: true,
-  //         labels: true,
-  //         pull_requests: true,
-  //         releases: true,
-  //         wiki: true,
-  //         repo_owner: process.env.GITEA_USERNAME,
-  //         mirror: true,
-  //         private: ghRepo.private,
-  //       });
-  //       if (!migrateResponse.ok) {
-  //         console.log("failed to migrate github repo", ghRepo.http_url, migrateResponse);
-  //         throw migrateResponse.error;
-  //       }
-  //       console.log("repo migrated, sleeping", ghRepo.name, migrateResponse);
-  //       await sleep(sleepTimeMs);
-  //     } else {
-  //       console.log("gitea call to get repo failed", err);
-  //       throw err;
-  //     }
-  //   }
-  //   // if (ghRepo.private) {
-  //   //   console.log("found a private repo", ghRepo);
-  //   //   break;
-  //   // }
-  // }
 }
 
 run().then(() => {
